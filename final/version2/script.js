fc2f8de49caa35dca38424944b58e007b96e063d
egwen <egwen@ucdavis.edu>
2023-03-11 16:38:54 -0800

    increment/decrement resource animations

--- final/version2/script.js
+++ final/version2/script.js
@@ -2,6 +2,8 @@
     'use strict';
     console.log("reading js");
 
+    let gameStarted = false;
+
     // Sound effects
     const plantSound = new Audio('sounds/plant.wav');
     const buttonSound = new Audio('sounds/button.wav');
@@ -51,7 +53,7 @@
             sun: 0,
             fertilizer: 0
         },
-        day: 0,
+        day: 1,
         gameEnd: 30
     }
 
@@ -120,14 +122,13 @@
         for (let resource in gameData.health) {
             gameData.health[resource] = randomInt(optimalHealth[resource]);
         }
-        // Reset the day to 0
-        gameData.day = 0;
+        // Reset the day to 1
+        gameData.day = 1;
 
         updateHealth();
+        gameStarted = true;
 
         // setUpDay();
-
-        
     }
 
     function flipDate(front, back) {
@@ -176,6 +177,8 @@
 
         // Each resource has a probability of depleting by 1 or 2 (See depletionRate for exact probabilities)
         for (let r in gameData.health) {
+            document.querySelector(`#${r}-stat span`).textContent = gameData.health[r];
+
             if (gameData.health[r] > 0 && (Math.random() < depletionRate[r])) {
                 gameData.health[r] -= randomInt([1,3]);
                 if (gameData.health[r] < 0) {
@@ -186,12 +189,13 @@
         updateHealth();
         gameData.day++;
 
+        // Check if game has ended
+        // Plant survived the 30 days
         if (gameData.day > 30) {
             document.getElementById('end-survive').className = 'showing';
             endGame();
-
         }
-
+        // Plant died before the 30 days
         if (calculateHealthVal() == 0) {
             document.getElementById('end-dead').className = 'showing';
             endGame();
@@ -203,8 +207,6 @@
         gameData.currentResources.forEach((r, i) => {
             gameData.currentResources[i] = Math.floor(Math.random() * gameData.resources.length);
         });
-        // resourceCards[0].textContent = gameData.resources[gameData.resource1];
-        // resourceCards[1].textContent = gameData.resources[gameData.resource2];
 
         // Get resource type and amount
         let resourceType = gameData.currentResources.map((r) => {
@@ -214,7 +216,10 @@
             return gameData.resources[r][1];
         })
 
+
         for (let i = 0; i < 2; i++ ){
+            document.querySelector(`#${resourceType[i]}-stat span`).textContent = gameData.health[resourceType[i]];
+
             // Update Plant health with corresponding resource type and amount
             gameData.health[resourceType[i]] += resourceAmount[i];
 
@@ -223,67 +228,33 @@
 
             resourceCardsBack[i].appendChild(img);
             console.log("back", resourceCardsBack[i])
-            // resourceCardsFront[i].appendChild(img);
         }
 
         console.log(resourceCardsBack)
-        // let img1 = document.createElement('img');
-        // img1.src = `images/${resourceType1}${resourceAmount1}.png`;
-        // resourceCardsFront[0].appendChild(img1);
-        // let img2 = document.createElement('img');
-        // img2.src = `images/${resourceType2}${resourceAmount2}.png`;
-        // resourceCardsFront[1].appendChild(img2);
 
- 
-        
         updateHealth();
     }
 
     function calculateHealthVal() {
         let health = 30;
 
-        // Optimal range => no deduction
-        // Water health
-        if (gameData.health.water < optimalHealth.water[0]) {
-            health -= optimalHealth.water[0] - gameData.health.water;
-        } else if (gameData.health.water > optimalHealth.water[1]) {
-            health -= gameData.health.water - optimalHealth.water[1];
+        // If resource amount within optimal range => no health deduction
+        // Otherwise, deduct the difference
+        for (let r in gameData.health) {
+            if (gameData.health[r] < optimalHealth[r][0]) {
+                health -= optimalHealth[r][0] - gameData.health[r];
+            } else if (gameData.health[r] > optimalHealth[r][1]) {
+                health -= gameData.health[r] - optimalHealth[r][1];
+            }
         }
-
-        console.log("heath after water", health);
-
-        // Sunlight health
-        if (gameData.health.sun < optimalHealth.sun[0]) {
-            health -= optimalHealth.sun[0] - gameData.health.sun;
-        } else if (gameData.health.sun > optimalHealth.sun[1]) {
-            health -= gameData.health.sun - optimalHealth.sun[1];
-        }
-        console.log("heath after sun", health);
-
-        // fertilizer health
-        if (gameData.health.fertilizer < optimalHealth.fertilizer[0]) {
-            health -= optimalHealth.fertilizer[0] - gameData.health.fertilizer;
-        } else if (gameData.health.fertilizer > optimalHealth.fertilizer[1]) {
-            health -= gameData.health.fertilizer - optimalHealth.fertilizer[1];
-        }
-        console.log("heath after fertilizer", health);
-
         return health > 0 ? health : 0;
     }
 
     function updateHealth() {
         console.log("update health", health);
 
-
-
-        // healthStats.textContent = '';
-
-        // for (let r in gameData.health) {
-        //     healthStats.textContent += `${r}: ${gameData.health[r]}\n`;
-        // }
-
         let healthVal = calculateHealthVal() / 30;
-        console.log(healthVal);
+        // console.log(healthVal);
         updatePlantVisuals(healthVal);
         updateHealthColor(healthVal);
 
@@ -298,9 +269,41 @@
             healthFill.style.width = `${width * healthVal}px`;
         }
 
-        // Stats bars styling
+        // Stats overlay styling
         for (let r in gameData.health) {
-            document.getElementById(`${r}-stat`).textContent = gameData.health[r];
+            healthOverlay.className = 'showing';
+
+            if (gameStarted) {
+                iterateStat(r);
+            } else {
+                document.querySelector(`#${r}-stat span`).textContent = gameData.health[r];
+            }
+        }
+    }
+
+    function iterateStat(resource) {
+        let stat = document.getElementById(`${resource}-stat`);
+        let statValue = document.querySelector(`#${resource}-stat span`);
+
+        if (gameStarted && gameData.health[resource] != stat.textContent) {
+            setTimeout(() => {
+                if (statValue.textContent < gameData.health[resource]) {
+                    stat.style.animation = 'increment 0.75s ease forwards';
+                } else {
+                    stat.style.animation = 'decrement 0.75s ease forwards';
+                }
+
+                stat.addEventListener('animationend', () => {
+                    stat.style.animation = ''
+                })
+
+                if (statValue.textContent < gameData.health[resource]) {
+                    statValue.textContent = parseInt(statValue.textContent) + 1;
+                } else {
+                    statValue.textContent = parseInt(statValue.textContent) - 1;
+                }
+                iterateStat(resource);
+            }, 1000)
         }
     }
 
@@ -311,14 +314,12 @@
 
         const plant = document.querySelector('#plant-visual img');
 
-        // console.log(plant.src, plant.src.includes(`plant${index}.png`));
         if (!plant.src.includes(`plant${index}.png`)) {
             plantSound.play();
         }
 
         plant.src = `images/plant${index}.png`;
 
-        
         // Update Health Bar Text
         document.getElementById('bar-outline').textContent = healthText[index];
 
@@ -343,12 +344,12 @@
     });
 
     function endGame() {
+        gameStarted = false;
         overlay.style.display = 'flex';
         overlay.className = 'showing;'
         healthBar.style.display = 'none'
         console.log("END GAME");
         document.getElementById('end-survive').className = 'showing';
     }
-
 })(); 
     
